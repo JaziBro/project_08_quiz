a02f43565a0731be4b229605812e51ca92177116
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2023-10-13 08:25:42 -0700

    .

--- main.ts
+++ main.ts
@@ -10,56 +10,88 @@ let answers = await inquirer.prompt([
     {
         name: "question2",
         type: "list",
-        choices: ["A) Atlantic Ocean", "B) Inidan Ocean", "C) Pacific Ocean", "D) Southern Ocean "],
+        choices: ["A) Atlantic Ocean", "B) Inidan Ocean", "C) Pacific Ocean", "D) Southern Ocean"],
         message: "Which of the following is the largest Ocean on Earth: "        
     },
     {
         name: "question3",
         type: "list",
-        choices: ["A) North", "B) why would it have smoke", "C) West", "D) none of the above "],
+        choices: ["A) North", "B) why would it have smoke", "C) West", "D) none of the above"],
         message: "If an electric train is travelling towards North, where will its smoke travel?: "        
     },
     {
         name: "question4",
         type: "list",
-        choices: ["A) Europe", "B) Asia", "C) Southern America", "D) Northern America "],
+        choices: ["A) Europe", "B) Asia", "C) Southern America", "D) Northern America"],
         message: "Which of the following is the largest Continent of Earth: "        
     },
     {
         name: "question5",
         type: "list",
-        choices: ["A) 4th", "B) 2nd", "C) 3rd", "D) None of the above "],
+        choices: ["A) 4th", "B) 2nd", "C) 3rd", "D) None of the above"],
         message: "You're 4th place right now in a race. What place will you be in when you pass the person in 3rd place?: "        
     },
     {
         name: "question6",
         type: "list",
-        choices: ["A) Atlantic Ocean", "B) Inidan Ocean", "C) Pacific Ocean", "D) Southern Ocean "],
-        message: "Which of the following is the largest Ocean on Earth: "        
+        choices: ["A) Russia", "B) Portugal", "C) China", "D) India"],
+        message: "Which of the following is the largest Country (by population) on Earth: "        
     },
     {
         name: "question7",
         type: "list",
-        choices: ["A) Atlantic Ocean", "B) Inidan Ocean", "C) Pacific Ocean", "D) Southern Ocean "],
-        message: "Which of the following is the largest Ocean on Earth: "        
+        choices: ["A) Amazon River", "B) River Nile", "C) Congo River", "D) Yellow River"],
+        message: "Which of the following is the largest River on Earth: "        
     },
     {
         name: "question8",
         type: "list",
-        choices: ["A) Atlantic Ocean", "B) Inidan Ocean", "C) Pacific Ocean", "D) Southern Ocean "],
-        message: "Which of the following is the largest Ocean on Earth: "        
+        choices: ["A) Great Wall of China", "B) Great Pyramid", "C) Taj Mahal", "D) Time Square"],
+        message: "Which of the following is not a Wonder of the World: "        
     },
     {
         name: "question9",
         type: "list",
-        choices: ["A) Atlantic Ocean", "B) Inidan Ocean", "C) Pacific Ocean", "D) Southern Ocean "],
-        message: "Which of the following is the largest Ocean on Earth: "        
+        choices: ["A) China", "B) Saudi Arabia", "C) Australia", "D) Russia"],
+        message: "Which of the following is the largest Country (by area) on Earth: "        
     },
     {
         name: "question10",
         type: "list",
-        choices: ["A) Atlantic Ocean", "B) Inidan Ocean", "C) Pacific Ocean", "D) Southern Ocean "],
-        message: "Which of the following is the largest Ocean on Earth: "        
+        choices: ["A) Mars", "B) Mercury", "C) Jupiter", "D) Earth"],
+        message: "Which of the following is the largest Planet: "        
     },
-    
-])
+    ])
+
+var score = 0
+if(answers.question1 === "A) Mount Everest"){
+    score += 1
+}
+if(answers.question2 === "C) Pacific Ocean"){
+    score += 1
+}
+if(answers.question3 === "B) why would it have smoke"){
+    score += 1
+}
+if(answers.question4 === "B) Asia"){
+    score += 1
+}
+if(answers.question5 === "C) 3rd"){
+    score += 1
+}
+if(answers.question6 === "C) China"){
+    score += 1
+}
+if(answers.question7 === "B) River Nile"){
+    score += 1
+}
+if(answers.question8 === "D) Time Square"){
+    score += 1
+}
+if(answers.question9 === "D) Russia"){
+    score += 1
+}
+if(answers.question10 === "D) Jupiter"){
+    score += 1
+}
+console.log(`Your Total Score Out Of 10 is ${score}!`)
